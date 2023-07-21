cb60f10f0bab8d956062e2e1cc000721f0025c2f
yangzhenyu900 <15001207194@163.com>
2023-07-21 18:10:38 +0800

    Update running-devlopment.sh

--- running-devlopment.sh
+++ running-devlopment.sh
@@ -2,7 +2,7 @@ running=`kubectl get pods -n devlopment|grep Running|awk '{print $3}'`> /dev/nul
 if [ "$running" == "Running" ]; then
    echo "continue"
 else
-  kubectl delete -f k8s-dev.yaml > /dev/null 2>&1
+  kubectl delete -f k8s-dev-harbor.yaml > /dev/null 2>&1
   if [ "$?" != 0 ]; then
     echo "continue"
   fi
