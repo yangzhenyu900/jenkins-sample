7d068669125f342e7bf6c2bde77c3d8e6ee53f79
yangzhenyu900 <15001207194@163.com>
2023-07-21 18:11:43 +0800

    Update running-qa.sh

--- running-qa.sh
+++ running-qa.sh
@@ -2,7 +2,7 @@ running=`kubectl get pods -n qatest|grep Running|awk '{print $3}'`> /dev/null 2>
 if [ "$running" == "Running" ]; then
    echo "continue"
 else
-  kubectl delete -f k8s-qa.yaml > /dev/null 2>&1
+  kubectl delete -f k8s-qa-harbor.yaml > /dev/null 2>&1
   if [ "$?" != 0 ]; then
     echo "continue"
   fi
