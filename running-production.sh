55ff546cd429ed7c47a7904b99bdb781c5616138
yangzhenyu900 <15001207194@163.com>
2023-07-21 18:11:21 +0800

    Update running-production.sh

--- running-production.sh
+++ running-production.sh
@@ -2,7 +2,7 @@ running=`kubectl get pods -n production|grep Running|awk '{print $3}'`> /dev/nul
 if [ "$running" == "Running" ]; then
    echo "continue"
 else
-  kubectl delete -f k8s-prod.yaml > /dev/null 2>&1
+  kubectl delete -f k8s-prod-harbor.yaml > /dev/null 2>&1
   if [ "$?" != 0 ]; then
     echo "continue"
   fi
